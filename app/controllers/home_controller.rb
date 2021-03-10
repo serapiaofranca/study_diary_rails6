ee3d5cdd2b016933b69f7b5bfb196471ac7d9d9a
Rogerio A Serapiao <rserapa@gmail.com>
2021-03-09 21:59:31 -0300

    remove model study_item manual implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
 
     def index
         #render 'index'  -- por padrao o rails busca essa view
-        @study_items = []
-        @study_items << StudyItem.new("MVC")
-        @study_items << StudyItem.new("Http")
-        @study_items << StudyItem.new("Gems")
+        @study_items = StudyItem.all()
     end
 end
